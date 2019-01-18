c928c5719b1d5e799ea1a7e707768ac009338ada
emzhofb <ikhdamuhammad@gmail.com>
2019-01-18 21:48:17 +0700

    update tugas

--- week2/Exercise Menggabungkan Konsep Perulangan dan Kondisional.js
+++ week2/Exercise Menggabungkan Konsep Perulangan dan Kondisional.js
@@ -39,10 +39,50 @@ Masih ingat dengan Proxytia? game yang pernah kamu buat pada tugas sebelumnya. P
     Jika sebaliknya, console.log('Sayang sekali, ' + peran + ' ' + nama + ' dikalahkan monster...')
 */
 
+var nama = 'Zoro';
+var peran = 'Penyihir';
+var tahunLahir = 1999;
+var umur = 2019 - tahunLahir;
+var playerHealth = tahunLahir * Math.random();
+var monsterHealth = tahunLahir * Math.random();
+var kodeMonster = Math.floor(Math.pow(100, Math.random()));
+
+if ( nama === '' ) {
+    peran = 'Kacung';
+} else if ( nama !== '' && peran === '' ) {
+    console.log ( 'Halo ' + nama + ', Pilih peranmu untuk memulai game!' );
+} else if ( nama !== '' && peran === 'Ksatria' ) {
+    console.log ( 'Selamat datang di Dunia Proxytia, ' + nama + '\n' + 'Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!' );
+} else if ( nama !== '' && peran === 'Tabib' ) {
+    console.log ( 'Selamat datang di Dunia Proxytia, ' + nama + '\n' + 'Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.' );
+} else if ( nama !== '' && peran === 'Penyihir' ) {
+    console.log ( 'Selamat datang di Dunia Proxytia, ' + nama + '\n' + 'Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!' );
+}
+
+for ( let i = 1; i <= tahunLahir; i++ ) {
+    if ( i % umur === 0 ) {
+        console.log(peran + ' ' + nama + ' menyerang monster!');
+    } else if ( i % kodeMonster === 0 ) {
+        console.log('monster menyerang ' + peran + ' ' + nama + '!');
+        playerHealth -= kodeMonster;
+    } else if ( i % umur === 0 && i % kodeMonster === 0 ) {
+        console.log('Health keduanya bertambah');
+        playerHealth += kodeMonster;
+        monsterHealth += umur;
+    }
+}
+
+if ( playerHealth > monsterHealth ) {
+    console.log('Selamat, ' + peran + ' ' + nama + ' memenangkan pertarungan!');
+} else {
+    console.log('Sayang sekali, ' + peran + ' ' + nama + ' dikalahkan monster...');
+}
+
+/*
 const nama = 'Zoro';
 let peran = 'Penyihir';
 let tahunLahir = 1999;
-const umur = 2017 - tahunLahir;
+const umur = 2019 - tahunLahir;
 let playerHealth = tahunLahir * Math.random();
 let monsterHealth = tahunLahir * Math.random();
 const kodeMonster = Math.floor(Math.pow(100, Math.random()));
@@ -77,3 +117,4 @@ if ( playerHealth > monsterHealth ) {
 } else {
     console.log('Sayang sekali, ' + peran + ' ' + nama + ' dikalahkan monster...');
 }
+*/
